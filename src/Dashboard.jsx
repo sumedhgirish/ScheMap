95f356cb1b42ef054970542b5dd321227cb653bd
Sumedh Girish <sumedh.girish@gmail.com>
2025-11-10 23:40:41 +0530

    Added Dashboard v1.0
    Signed-off-by: Sumedh Girish <sumedh.girish@gmail.com>

---
+++ src/Dashboard.jsx
@@ -0,0 +1,55 @@
+import "./Dashboard.css";
+
+function NavBar() {
+  return (
+    <div className="dbnavbar">
+      <span className="brand">
+        <img src="src/assets/logo.png" className="logo" alt="Logo" />
+        <p className="title">Home</p>
+      </span>
+      <span className="links">
+        <a className="redirect_link">About</a>
+        <a className="redirect_link">Profile</a>
+        <a className="redirect_link">Projects</a>
+      </span>
+    </div>
+  );
+}
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="heroImage">
+        <div className="heroText">
+          <h1 className="herocont">Sche-Map</h1>
+          <p className="herodesc">Schedule. Map. Repeat.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function FeatureCard({ title, content }) {
+  return (
+    <div className="featureCard">
+      <h2 className="featuretitle">{title}</h2>
+      <p className="featurecontent">{content}</p>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <div className="features">
+        <FeatureCard title="Test Title" content="Test Content" />
+        <FeatureCard title="Test Title" content="Test Content" />
+        <FeatureCard title="Test Title" content="Test Content" />
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
